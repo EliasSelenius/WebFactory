77ad54a70d4881ff471011db28f2e38e1ce2eb58
Elias Sebastian Haneborg <elias@nextron.no>
2018-10-16 13:55:26 +0200

    Inital commit

---
+++ WebFactory/Css.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebFactory {
+    public static class Css {
+
+        public static string Grid(int cols, int rows) {
+            string content = @"html, body { height: 100%; }";
+
+            for (int c = 1; c <= cols; c++) {
+                content += $".col-{c}" + "{ \n width: " + ((100f / cols) * c).ToString() + "%; \n } \n";
+            }
+
+            for (int i = 1; i <= cols; i++) {
+                content += $".col-{i},";
+            }
+            content = content.Remove(content.Length - 1);
+            content += @"{ float: left; }";
+
+
+            for (int r = 1; r <= rows; r++) {
+                content += $".row-{r}" + "{ \n height: " + ((100f / rows) * r).ToString() + "%; \n } \n";
+            }
+
+
+            return content;
+        }
+    }
+}
---
+++ WebFactory/Html.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebFactory {
+    public static class Html {
+
+        private static Color color;
+
+        public static void Fill(Color c) {
+            color = c;
+        }
+
+        public static IHtmlString Rect(float x, float y, float w, float h) {
+
+
+            string res = $"<div style=\"transform: translate({x}px, {y}px); " +
+                                      $"width: {w}px;" +
+                                      $"height: {h}px;" +
+                                      $"background-color: rgb({color.R}, {color.G}, {color.B});" +
+                                      $"position: absolute;\"></div>";
+
+            return new System.Web.HtmlString(res);
+        }
+    }
+}
